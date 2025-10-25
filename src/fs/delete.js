e0c66e1e8030abf0e9b627c766dd68a6e816cd16
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-10-25 17:51:08 +0400

    fea: create file deletion function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -2,28 +2,29 @@ import { unlink, access } from "node:fs/promises";
 const remove = async () => {
   // Write your code here
   const filePath = "./files/fileToRemove.txt";
+  const errorMessage = "FS operation failed";
   try {
+    //check if file exists
     try {
       await access(filePath);
     } catch (e) {
       throw new Error(errorMessage);
     }
-    await unlink(filePath);
-    console.log("File deleted successfully");
+
+    //after ensuring in file existence try to delete it
+    //and handle if there is any error
+    try {
+      await unlink(filePath);
+      console.log("File deleted successfully");
+    } catch (e) {
+      console.log(e);
+    }
   } catch (e) {
+    //handle both of custom thrown error and uknown one
     if (e instanceof Error && e.message === errorMessage) {
       console.log(e.message);
     } else {
-      try {
-        await renamePromise(oldPath, newPath);
-      } catch (e) {
-        console.log(e);
-      }
-    }
-    if (err.code === "ENOENT") {
-      console.log("File does not exist");
-    } else {
-      console.error("Error deleting file:", err);
+      console.log(e);
     }
   }
 };
