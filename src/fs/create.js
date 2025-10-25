a6d30c23930ba6897aab4ab9a67285ab04e02cb1
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-10-25 10:51:39 +0400

    feat: add file creating logic in create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,12 @@
+import { writeFile } from "node:fs/promises";
 const create = async () => {
   // Write your code here
+  try {
+    const content = "I am fresh and young";
+    await writeFile("./files/fresh.txt", content);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 await create();
