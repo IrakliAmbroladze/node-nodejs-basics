7f5624327fa1537dfbcaaf8eba8d6b072ab7c77f
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-10-25 12:14:17 +0400

    feat: add logic for checking file existence before writing

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,11 +1,22 @@
-import { writeFile } from "node:fs/promises";
+import { writeFile, access } from "node:fs/promises";
 const create = async () => {
   // Write your code here
+  const content = "I am fresh and young";
+  const file = "./files/fresh.txt";
+  const errorMessage = "FS operation failed";
   try {
-    const content = "I am fresh and young";
-    await writeFile("./files/fresh.txt", content);
+    await access(file);
+    throw new Error(errorMessage);
   } catch (e) {
-    console.log(e);
+    if (e instanceof Error && e.message === errorMessage) {
+      console.log(e.message);
+    } else {
+      try {
+        await writeFile(file, content);
+      } catch (e) {
+        console.log(e);
+      }
+    }
   }
 };
 
