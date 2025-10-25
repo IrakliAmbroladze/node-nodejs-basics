a5c8d71145b3572bff1468f09a2765682d0bcebd
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-10-25 13:34:24 +0400

    feat: implement folder coping
    If the folder already exists error is thrown

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { cp } from "node:fs/promises";
 const copy = async () => {
   // Write your code here
+  const src = "./files/";
+  const dest = "./files_copy";
+  const errorMessage = "FS operation failed";
+  try {
+    try {
+      await cp(src, dest, {
+        errorOnExist: true,
+        recursive: true,
+        force: false,
+      });
+    } catch (e) {
+      throw new Error(errorMessage);
+    }
+  } catch (e) {
+    if (e instanceof Error && e.message === errorMessage) {
+      console.log(e.message);
+    } else {
+      console.log(e);
+    }
+  }
 };
 
 await copy();
