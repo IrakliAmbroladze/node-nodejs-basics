e8c112e9d51d356331213a70efc2adbac5a4053c
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-10-27 00:23:37 +0400

    feat: create worker threads based on cpu cores quantity

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,8 +1,32 @@
 import { cpus } from "node:os";
+import { Worker } from "node:worker_threads";
+import { URL, fileURLToPath } from "node:url";
+
 const performCalculations = async () => {
   // Write your code here
   const numCPUs = cpus().length;
-  console.log(numCPUs);
+  const workerURL = new URL("./worker.js", import.meta.url);
+  const workerPath = fileURLToPath(workerURL);
+
+  const promises = Array.from({ length: numCPUs }, (_, i) => {
+    const workerData = 10 + i;
+    const worker = new Worker(workerPath, { workerData });
+
+    return new Promise((resolve) => {
+      worker.on("message", (data) => {
+        resolve({ status: "resolved", data });
+      });
+      worker.on("error", () => {
+        resolve({ status: "error", data: null });
+      });
+      worker.on("exit", (code) => {
+        if (code !== 0) resolve({ status: "error", data: null });
+      });
+    });
+  });
+
+  const resultsArray = await Promise.all(promises);
+  console.log(resultsArray);
 };
 
 await performCalculations();
