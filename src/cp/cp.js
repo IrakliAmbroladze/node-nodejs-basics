0a49c3663105e08c397d2ccb0c7011b3ae9942a0
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-10-27 01:51:59 +0400

    feat: spawn child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { spawn } from "node:child_process";
+import { URL, fileURLToPath } from "node:url";
 const spawnChildProcess = async (args) => {
   // Write your code here
+  const scriptURL = new URL("./files/script.js", import.meta.url);
+  const scriptPath = fileURLToPath(scriptURL);
+  const child = spawn("node", [scriptPath, ...args], {
+    stdio: ["pipe", "pipe", "inherit"],
+  });
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2", "other argument"]);
