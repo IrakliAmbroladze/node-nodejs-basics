8835ef1146b68652800e15c0ee6898df0cc53ea0
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-10-26 23:37:18 +0400

    feat(wt): import cpu to get the number of cpus

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,8 @@
+import { cpus } from "node:os";
 const performCalculations = async () => {
   // Write your code here
+  const numCPUs = cpus().length;
+  console.log(numCPUs);
 };
 
 await performCalculations();
