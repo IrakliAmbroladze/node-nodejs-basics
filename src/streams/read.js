40afeeded1f6d290f25b6b99d2a6bf91d6020ecc
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-10-26 17:52:25 +0400

    feat: create readbale stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { createReadStream } from "node:fs";
+import { URL, fileURLToPath } from "node:url";
 const read = async () => {
   // Write your code here
+  const fileURL = new URL("./files/fileToRead.txt", import.meta.url);
+  const filePath = fileURLToPath(fileURL);
+
+  const readableStream = createReadStream(filePath, { encoding: "utf-8" });
+
+  readableStream.pipe(process.stdout);
 };
 
 await read();
